272fa8247148e8fcfbfbb9e9a553a4a146b1a069
FelipeBarrosCruz <felipe.cruz@hash.com.br>
2022-02-23 10:24:15 -0300

    add name route

--- main.go
+++ main.go
@@ -24,5 +24,12 @@ func main() {
 			"name": "bar-service",
 		})
 	})
+
+	r.GET("/name", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"name": "Camila",
+		})
+	})
+
 	r.Run(PORT) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
